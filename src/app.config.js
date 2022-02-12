da0269f03a5d3e144e6d26f69fa0bb35f10fa5fa
rojer <rojerchen@qq.com>
2022-02-12 13:25:15 +0800

    feat: :boom: 更新demo

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-  pages: ["pages/index/index"],
+  pages: ["pages/index/index", "pages/katex/index"],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
---
+++ src/pages/katex/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { View, Textarea, Button } from "@tarojs/components";
+import Katex from "../../components/katex";
+import "./index.less";
+
+export default () => {
+  const [latex, setLatex] = useState(
+    "\\displaystyle \\left( \\sum_{k=1}^n a_k b_k \\right)^2 \\leq \\left( \\sum_{k=1}^n a_k^2 \\right) \\left( \\sum_{k=1}^n b_k^2 \\right)"
+  );
+  const [input, setInput] = useState(latex);
+
+  return (
+    <View className="container">
+      <View className="latex">
+        <Katex
+          latex={latex}
+          option={{
+            displayMode: true,
+            throwOnError: false
+          }}
+        />
+      </View>
+      <Textarea
+        value={input}
+        maxlength={-1}
+        onInput={e => {
+          setInput(e.target.value);
+        }}
+        autoHeight
+      />
+      <Button
+        onClick={() => {
+          setLatex(input);
+        }}
+      >
+        渲染
+      </Button>
+    </View>
+  );
+};
